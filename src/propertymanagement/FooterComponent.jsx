305d31b11305732466f5a612e793bf1703c6dad0
blakeeversole <blakeceversole@gmail.com>
2021-03-10 17:16:48 -0500

    changes

--- src/propertymanagement/FooterComponent.jsx
+++ src/propertymanagement/FooterComponent.jsx
@@ -4,7 +4,7 @@ class FooterComponent extends Component{
     render(){
         return(
             <footer className="footer text-center">
-                 <span className="text-muted">All Rights Reserved 2021 Blake Eversole</span>
+                 <span className="text-muted">All Rights Reserved 2021 Blake Eversole.</span>
             </footer>
         )
     }
