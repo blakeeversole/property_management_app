809cf027edcec9fcfa9ed1ee916d7c75e30c31d1
blakeeversole <blakeceversole@gmail.com>
2021-03-29 12:37:13 -0400

    changes

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,9 @@
-export const API_URL = 'http://localhost:8080'
-export const JPA_API_URL = 'http://localhost:8080/jpa'
+// export const API_URL = 'http://localhost:8080'
+// export const JPA_API_URL = 'http://localhost:8080/jpa'
+export const API_URL = 'https://restful-api.azurewebsites.net'
+export const JPA_API_URL = 'https://restful-api.azurewebsites.net/jpa'
+// export const API_URL = 'https://propertymanagementapplication-apim2.developer.azure-api.net'
+// export const JPA_API_URL = 'https://propertymanagementapplication-apim2.developer.azure-api.net/jpa'
 export const Role = {
     Employee: 'Employee',
     Tenant: 'Tenant',
